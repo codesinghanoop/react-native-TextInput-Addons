d296a38bcdea9e7370a84bce78437d1549be6139
Anoop Singh <anoop100singh@gmail.com>
2017-05-20 04:08:15 +0530

    Example Fixes

--- example/Example/example.js
+++ example/Example/example.js
@@ -29,7 +29,8 @@ export default class CrossButtonExample extends Component {
    this.setState({ nameLoading: !this.state.nameLoading});
  }
   render() {
-
+    let emailButtonText = this.state.emailLoading? (<Text style={styles.buttonTextStyle}>Stop Loading Emails</Text>) : (<Text style={styles.buttonTextStyle}>Start Loading Emails</Text>)
+    let nameButtonText = this.state.nameLoading? (<Text style={styles.buttonTextStyle}>Stop Loading Names</Text>) : (<Text style={styles.buttonTextStyle}>Start Loading Names</Text>)
     return (
 
       <View style = {styles.container}>
@@ -37,11 +38,11 @@ export default class CrossButtonExample extends Component {
          containerStyle= {{backgroundColor: 'green'}} onInputCleared={this.onEmailInputCleared} />
        <CrossTextInput underlineColorAndroid="transparent" hideProgress={false} isLoading= {this.state.nameLoading} value = {this.state.name} style={styles.textInput} autoCorrect={false} autoCapitalize="none" keyboardType="email-address" placeholder={'NAME'} placeholderTextColor="#adadad" onChangeText={(text) => this.setState({ name: text })}
         containerStyle= {{backgroundColor: 'red'}} onInputCleared={this.onNameInputCleared} />
-      <TouchableOpacity onPress={() => this.fetchingEmail()}  underlayColor={'#00000000'}>
-          <Text style={styles.buttonTextStyle}>Stop Loading Emails</Text>
+      <TouchableOpacity style= {styles.buttonStyle} onPress={() => this.fetchingEmail()}  underlayColor={'#00000000'}>
+          {emailButtonText}
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.fetchingName()}  underlayColor={'#00000000'}>
-          <Text style={styles.buttonTextStyle}>Stop Loading Emails</Text>
+        <TouchableOpacity style= {styles.buttonStyle} onPress={() => this.fetchingName()}  underlayColor={'#00000000'}>
+          {nameButtonText}
         </TouchableOpacity>
       </View>
 
@@ -68,18 +69,18 @@ const styles = StyleSheet.create({
 
   buttonStyle: {
     height: 50,
-    width: 124,
-    flex: 1,
+    width: 300,
     flexDirection: 'row',
-    backgroundColor: 'rgba(0,0,0,0)',
+    backgroundColor: 'red',
     borderColor: 'red',
     borderWidth: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
+    alignSelf: 'center',
+    justifyContent: 'center',
+    margin: 10,
   },
 
   buttonTextStyle: {
-    fontSize: 12,
+    fontSize: 15,
     color: 'green',
     alignSelf: 'center',
     fontFamily: 'System',
